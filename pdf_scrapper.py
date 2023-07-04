656d308a3bd029c4272f873389f8a2e06adf189f
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-07-04 17:17:41 -0300

    pdf_pattern1 scraper done

--- pdf_scrapper.py
+++ pdf_scrapper.py
@@ -1,33 +1,34 @@
 import pandas as pd
+import pdfplumber
 import pdfquery
 
 
-def pdfscrape_pattern1(pdf, pagecount):
+def pdfscrape_infojogo_pattern1(pdf):
   #Extracting each relevant information individually
-  for count in range(pagecount - 1):
-    if pdf.pq('LTTextLineHorizontal:contains("{}")'.format("ALUGUEIS E SEGUROS")):
-      index = 3
-    else: 
-      index = 0
+  # for count in range(pagecount - 1):
+  #   if pdf.pq('LTTextLineHorizontal:contains("{}")'.format("ALUGUEIS E SEGUROS")):
+  #     index = 3
+  #   else: 
+  #     index = 0
 
-  total =  pdf.pq('LTTextLineHorizontal:contains("{}")'.format("TOTAL"))[index] 
-  x0 = float(total.get('x0',0)) + 156.4
-  y0 = float(total.get('y0',0))
-  x1 = float(total.get('x1',0)) + 155.41
-  y1 = float(total.get('y1',0))
-  total_disponivel = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","")
+  # total =  pdf.pq('LTTextLineHorizontal:contains("{}")'.format("TOTAL"))[index] 
+  # x0 = float(total.get('x0',0)) + 156.4
+  # y0 = float(total.get('y0',0))
+  # x1 = float(total.get('x1',0)) + 155.41
+  # y1 = float(total.get('y1',0))
+  # total_disponivel = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","")
 
-  x0 = float(total.get('x0',0)) + 269.79
-  y0 = float(total.get('y0',0))
-  x1 = float(total.get('x1',0)) + 268.8
-  y1 = float(total.get('y1',0))
-  total_vendidos = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","")
+  # x0 = float(total.get('x0',0)) + 269.79
+  # y0 = float(total.get('y0',0))
+  # x1 = float(total.get('x1',0)) + 268.8
+  # y1 = float(total.get('y1',0))
+  # total_vendidos = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","")
 
-  x0 = float(total.get('x0',0)) + 387.83
-  y0 = float(total.get('y0',0))
-  x1 = float(total.get('x1',0)) + 398.516
-  y1 = float(total.get('y1',0))
-  renda = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","").replace(",",".")
+  # x0 = float(total.get('x0',0)) + 387.83
+  # y0 = float(total.get('y0',0))
+  # x1 = float(total.get('x1',0)) + 398.516
+  # y1 = float(total.get('y1',0))
+  # renda = pdf.pq('LTTextLineHorizontal:overlaps_bbox("%s, %s, %s, %s")' % (x0, y0, x1, y1)).text().replace(".","").replace(",",".")
 
   numero_jogo =  pdf.pq('LTTextLineHorizontal:contains("{}")'.format("RODADA"))[0] 
   x0 = float(numero_jogo.get('x0',0)) + 104.98
@@ -55,8 +56,41 @@ def pdfscrape_pattern1(pdf, pagecount):
       'Partida': int(partida),
       "Mandante": time_casa,
       "Visitante": time_fora,
-      "Ingressos Disponíveis": int(total_disponivel),
-      "Ingressos Vendidos": int(total_vendidos),
-      "Renda": float(renda),
+      # "Ingressos Disponíveis": int(total_disponivel),
+      # "Ingressos Vendidos": int(total_vendidos),
+      # "Renda": float(renda),
       }, index=[0])
-  return(page) 
+  return(page)
+
+
+def pdfscrape_infopublico_pattern1(pdf_path):
+  with pdfplumber.open(pdf_path) as pdf:
+    first_page = pdf.pages[0]
+    table = first_page.extract_table()
+
+  df = pd.DataFrame(table[1:], columns=table[0])
+  df.drop(df.columns[[0]], axis=1, inplace=True)
+  df.columns = ['INGRESSOS', 'DISPONÍVEL', 'DEVOLVIDOS', 'VENDIDOS', 'PREÇO', 'ARRECADAÇÃO']
+  df = df.drop(0)
+
+  inteiros = ['DISPONÍVEL', 'DEVOLVIDOS', 'VENDIDOS']
+  floats = ['PREÇO', 'ARRECADAÇÃO']
+
+  for inteiro in inteiros:
+    df[inteiro] = pd.to_numeric(df[inteiro].str.replace('.', ''), errors='coerce').astype('Int64')
+    
+  for floater in floats:
+    df[floater] = pd.to_numeric(df[floater].str.replace('[.,]', '', regex=True), errors='coerce')/100
+
+  df = df[df['INGRESSOS'] == 'TOTAL']
+
+  publico_pagante = df['VENDIDOS'].iloc[-1]
+  total_renda = df['ARRECADAÇÃO'].iloc[-1]
+
+  page = pd.DataFrame({
+    'Pagantes': [publico_pagante],
+    'Renda': [total_renda],
+  }, index=[0])
+
+  return(page)
+  
--- script.py
+++ script.py
@@ -7,14 +7,16 @@ from download_pdf import download_pdf_file
 import pdfquery
 from dotenv import load_dotenv
 
-from pdf_scrapper import pdfscrape_pattern1
+from pdf_scrapper import pdfscrape_infojogo_pattern1, pdfscrape_infopublico_pattern1
 
 load_dotenv()
 
 PDF_FOLDER_PATH = os.getenv("PDF_FOLDER_PATH")
 
-partidas = [66,67]
-tabela_publico = pd.DataFrame()
+partidas = [129, 9, 109, 107, 110]
+
+colunas = ['Competição', 'Rodada', 'Partida', 'Mandante', 'Visitante', 'Pagantes', 'Renda']
+tabela_publico = pd.DataFrame(columns=colunas)
 
 for partida in partidas:
     URL = f'https://conteudo.cbf.com.br/sumulas/2023/142{partida}b.pdf'
@@ -29,8 +31,12 @@ for partida in partidas:
         pagecount = pdf.doc.catalog['Pages'].resolve()['Count']
         for p in range(pagecount - 1):
             pdf.load(p)
-            page = pdfscrape_pattern1(pdf, pagecount) 
-            tabela_publico = tabela_publico.append(page, ignore_index=True).sort_values('Partida')
+            page_jogo = pdfscrape_infojogo_pattern1(pdf) 
+            
+        page_publico = pdfscrape_infopublico_pattern1(pdf_path)
+
+        page_merged = pd.concat([page_jogo, page_publico], axis=1)
+        tabela_publico = tabela_publico.append(page_merged, ignore_index=True).sort_values('Partida')
     
     remove_pdf_path = os.path.join(PDF_FOLDER_PATH, f'jogo_{partida}.pdf')
     remove_boletim_path = os.path.join(os.getcwd(), f'boletim_{partida}.txt')
@@ -39,5 +45,8 @@ for partida in partidas:
     
 # tabela_publico = tabela_publico.reset_index(drop=True, inplace=True)
 
-tabela_publico.to_csv('C:/src/python/CBF-Scraping/tabela_publico/tabela_publico.csv', mode='a',header=False, index=False)
+if(os.path.exists('C:/src/python/CBF-Scraping/tabela_publico/tabela_publico.csv')):
+    tabela_publico.to_csv('C:/src/python/CBF-Scraping/tabela_publico/tabela_publico.csv', mode='a',header=False, index=False)
+else: 
+    tabela_publico.to_csv('C:/src/python/CBF-Scraping/tabela_publico/tabela_publico.csv', mode='a',header=True, index=False)
 
